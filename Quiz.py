6560ce40f07cb41ed8c5cea7b3ccf55cf6f91a8e
Marianne_Joeriddine_L._Sawitan <mariannejoeriddine@gmail.com>
2023-10-12 19:44:32 +0800

    First Commit

---
+++ Quiz.py
@@ -0,0 +1,11 @@
+#This program will compute the total amount of diesel
+#Values of the Variables 
+price = 70
+liter = 7
+
+#Formula for Computing the total amount to pay
+total = price * liter
+
+#Display
+print("The total amount to pay is: ", total)
+
